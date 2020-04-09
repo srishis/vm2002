7f3bd4ac394ecb2e53a7cfea37aeba3fc244586c
Rishitosh Sawant <risawant@mo.ece.pdx.edu>
2020-04-08 21:23:26 -0700

    fixed package

--- vm2002_pkg.svh
+++ vm2002_pkg.svh
@@ -29,26 +29,26 @@ package vm2002_pkg;
   // struct for item count register in the vending machine
   // setting all items default count to avg value 8
   typedef struct packed {
-	logic WATER_COUNT	= 3'h8;
-  	logic COLA_COUNT	= 3'h8;
-  	logic PEPSI_COUNT	= 3'h8;
-  	logic FANTA_COUNT	= 3'h8;
-	logic COFFEE_COUNT	= 3'h8;
-	logic CHIPS_COUNT	= 3'h8;
-	logic BARS_COUNT	= 3'h8;
-	logic COOKIE_COUNT	= 3'h8;
+	logic [2:0] WATER_COUNT	 = 3'h8;
+  	logic [2:0] COLA_COUNT	 = 3'h8;
+  	logic [2:0] PEPSI_COUNT	 = 3'h8;
+  	logic [2:0] FANTA_COUNT	 = 3'h8;
+	logic [2:0] COFFEE_COUNT = 3'h8;
+	logic [2:0] CHIPS_COUNT	 = 3'h8;
+	logic [2:0] BARS_COUNT	 = 3'h8;
+	logic [2:0] COOKIE_COUNT = 3'h8;
   } item_count_struct_t;
   
   // struct for item costs register in the vending machine
   typedef struct packed {
-	logic COST_OF_WATER	= 8'h6;		// $0.50 
-  	logic COST_OF_COLA	= 8'h12;	// $1
-  	logic COST_OF_PEPSI	= 8'h12;	// $1
-  	logic COST_OF_FANTA	= 8'h12;	// $1
-	logic COST_OF_COFFEE	= 8'h24;	// $2
-	logic COST_OF_CHIPS	= 8'h15;	// $1.25
-	logic COST_OF_BARS	= 8'h18;	// $1.50
-	logic COST_OF_COOKIE	= 8'h21;	// $1.75
+	logic [7:0] COST_OF_WATER	= 8'h6;		// $0.50 
+  	logic [7:0] COST_OF_COLA	= 8'h12;	// $1
+  	logic [7:0] COST_OF_PEPSI	= 8'h12;	// $1
+  	logic [7:0] COST_OF_FANTA	= 8'h12;	// $1
+	logic [7:0] COST_OF_COFFEE	= 8'h24;	// $2
+	logic [7:0] COST_OF_CHIPS	= 8'h15;	// $1.25
+	logic [7:0] COST_OF_BARS	= 8'h18;	// $1.50
+	logic [7:0] COST_OF_COOKIE	= 8'h21;	// $1.75
   } cost_struct_t;
 
 
@@ -57,7 +57,7 @@ package vm2002_pkg;
 	A = 3'h0,			// Button A selects water			
   	B = 3'h1,			// Button B selects cola 			
   	C = 3'h2,			// Button C selects pepsi                       
-  	D = 3'h3,	stat		// Button D selects fanta                       
+  	D = 3'h3,			// Button D selects fanta                       
 	E = 3'h4,			// Button E selects coffee                       
 	F = 3'h5,			// Button F selects chips                       
 	G = 3'h6,			// Button G selects bars                       
