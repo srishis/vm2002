faf4b230d72d99cb3a11e6f07291730434d64b5e
vyawahare16 <vyawahareadiytya@gmail.com>
2020-04-14 12:38:19 -0700

    Update vm2002_tb.sv

--- vm2002_tb.sv
+++ vm2002_tb.sv
@@ -5,27 +5,13 @@ import vm2002_pkg::*;
 
 // output interface
 logic [2:0] product;
-<<<<<<< Updated upstream
-status_t status;
-=======
 logic [1:0] status;
->>>>>>> Stashed changes
 logic [15:0] balance;
 logic [7:0] info;
 
 logic clk, hrst, srst;
 
-<<<<<<< Updated upstream
-// user interface s
-coin_t coins; 
-items_t buttons; 
-logic select; 
 
-// supplier interface inputs
-item_t item;
-logic [3:0] count;
-cost_t cost;
-=======
 // user interface inputs
 logic [1:0] coins; 
 logic [2:0] buttons; 
@@ -35,7 +21,6 @@ logic select;
 logic [2:0] item;
 logic [3:0] count;
 logic [7:0] cost;
->>>>>>> Stashed changes
 logic valid;
 
 vm2002 VM(.*);
