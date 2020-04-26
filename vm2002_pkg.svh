5954b4c8b7beb5009f9b46002842825c3a8ef466
Rishitosh Sawant <srishis.214@gmail.com>
2020-04-25 19:50:07 -0700

    Update vm2002_pkg.svh

--- vm2002_pkg.svh
+++ vm2002_pkg.svh
@@ -1,3 +1,4 @@
+
 // package for common entities in vm2002 design and testbench
 
 `ifndef VM2002_PKG
@@ -6,12 +7,29 @@
 
 package vm2002_common_pkg;
 
+   // item count variables
+   logic [3:0] WATER_COUNT;	 
+   logic [3:0] COLA_COUNT;	 
+   logic [3:0] PEPSI_COUNT;	 
+   logic [3:0] FANTA_COUNT;	 
+   logic [3:0] COFFEE_COUNT; 
+   logic [3:0] CHIPS_COUNT;	 
+   logic [3:0] BARS_COUNT;	 
+   
+   // item cost variables
+   logic [7:0] COST_OF_WATER;	 
+   logic [7:0] COST_OF_COLA;	
+   logic [7:0] COST_OF_PEPSI;	
+   logic [7:0] COST_OF_FANTA;	
+   logic [7:0] COST_OF_COFFEE;	
+   logic [7:0] COST_OF_CHIPS;	
+   logic [7:0] COST_OF_BARS;	
+
   // enum for acceptable coins
   typedef enum logic [1:0] {
 	NICKEL      = 2'h1,
 	DIME        = 2'h2,
-	QUARTER     = 2'h3,
-	NO_COINS    = 2'h0	
+	QUARTER     = 2'h3
   }
   coins_t;
   
@@ -24,34 +42,7 @@ package vm2002_common_pkg;
 	COFFEE	= 3'h5,	
 	CHIPS	= 3'h6,	
 	BARS	= 3'h7	
-	//COOKIE	= 3'h7		
   } item_t;
-
-  // struct for item count register in the vending machine
-  // setting all items default count to avg value 8
-  typedef struct {
-	logic [2:0] WATER_COUNT	 = 3'h8;
-  	logic [2:0] COLA_COUNT	 = 3'h8;
-  	logic [2:0] PEPSI_COUNT	 = 3'h8;
-  	logic [2:0] FANTA_COUNT	 = 3'h8;
-	logic [2:0] COFFEE_COUNT = 3'h8;
-	logic [2:0] CHIPS_COUNT	 = 3'h8;
-	logic [2:0] BARS_COUNT	 = 3'h8;
-	//logic [2:0] COOKIE_COUNT = 3'h8;
-  } item_count_struct_t;
-  
-  // struct for item costs register in the vending machine
-  typedef struct {
-	logic [7:0] COST_OF_WATER	= 8'h6;		// $0.50 
-  	logic [7:0] COST_OF_COLA	= 8'h12;	// $1
-  	logic [7:0] COST_OF_PEPSI	= 8'h12;	// $1
-  	logic [7:0] COST_OF_FANTA	= 8'h12;	// $1
-	logic [7:0] COST_OF_COFFEE	= 8'h24;	// $2
-	logic [7:0] COST_OF_CHIPS	= 8'h15;	// $1.25
-	logic [7:0] COST_OF_BARS	= 8'h18;	// $1.50
-	//logic [7:0] COST_OF_COOKIE	= 8'h21;	// $1.75
-  } cost_struct_t;
-
 
   // enum for buttons on the vending machine
   typedef enum logic [2:0] {
@@ -66,10 +57,9 @@ package vm2002_common_pkg;
 
   // enum for product availability status
   typedef enum logic [1:0] {
-	AVAILABE      = 2'h1,
-	OUT_OF_STOCK  = 2'h2,
-	ERROR	      = 2'h3,
-	NO_STATUS     = 2'h0	
+	AVAILABLE      = 2'h1,
+	OUT_OF_STOCK   = 2'h2,
+	ERROR	       = 2'h3
   } status_t;
 
   // Indices for FSM states in Vending machine for bug free state logic 
@@ -80,7 +70,6 @@ package vm2002_common_pkg;
 	INSERT_COINS_INDEX     = 3,
 	CHECK_BALANCE_INDEX    = 4,
 	DISPENSE_ITEM_INDEX    = 5 
-	//SOFT_RESET             = 6 
   } state_index;
 
   // one hot FSM states for Vending machine
@@ -93,5 +82,7 @@ package vm2002_common_pkg;
 	DISPENSE_ITEM    = 6'b000001 << DISPENSE_ITEM_INDEX
   } fsm_state_t; 
 
+
 endpackage
+
 
